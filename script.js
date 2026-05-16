042c1af72c9439f38ac022c81555dfd3b76a4ba3
T4KOY4KII <[200~191971350+ZigmundSamarista@users.noreply.github.com>
2026-05-17 00:34:33 +0800

    -- new css and js updated --

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ const containerDiv = document.querySelector('.container');
 // Add 16 divs
 for (let i = 1; i < 257; i++) {
   const div = document.createElement('div');
-  containerDiv.appendChild(div).className = "grid-item";
+  div.className = "grid-item";
+  containerDiv.appendChild(div);
 
-  
+  div.addEventListener('mouseover', (e) => {
+    e.target.classList.add('my-colour-class');
+  });
 }
+  // Permanent hover effect
 
-// Permanent hover effect
-div.addEventListener('mouseover', 
-    e => e.target.classList.add('my-colour-class')
-)
+
